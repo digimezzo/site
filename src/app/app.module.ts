dd4f9098063c7577518f0cbfc4172dd9f850c76a
Digimezzo Raphaël <raphael@digimezzo.com>
2022-11-10 20:55:40 +0100

    Added backgrounds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { DonateComponent } from "./components/donate/donate.component";
 import { HomeComponent } from "./components/home/home.component";
 import { SoftwareComponent } from "./components/software/software.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { SoftwareComponent } from "./components/software/software.component";
     DonateComponent,
     ContactComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
