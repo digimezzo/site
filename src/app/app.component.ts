9e3b3459e5b7c7efc1a83223a861c41c029663b5
Digimezzo Raphaël <raphael@digimezzo.com>
2022-11-11 12:42:57 +0100

    Added pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,58 @@
-import { Component } from "@angular/core";
+import { Location } from "@angular/common";
+import { AfterViewInit, Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  public constructor(private router: Router, public location: Location) {}
+
+  public isHomeSelected: boolean = false;
+  public isSoftwareSelected: boolean = false;
+  public isDonateSelected: boolean = false;
+  public isContactSelected: boolean = false;
+
+  public ngOnInit(): void {
+    this.router.events.subscribe((event: any) => {
+      this.showPage();
+    });
+  }
+
+  public ngAfterViewInit(): void {
+    this.showPage();
+  }
+
+  public showPage(): void {
+    this.isHomeSelected = false;
+    this.isSoftwareSelected = false;
+    this.isDonateSelected = false;
+    this.isContactSelected = false;
+
+    switch (this.location.path()) {
+      case "/home":
+        this.isHomeSelected = true;
+
+        return;
+      case "/software":
+        this.isSoftwareSelected = true;
+
+        return;
+      case "/donate":
+        this.isDonateSelected = true;
+
+        return;
+      case "/contact":
+        this.isContactSelected = true;
+
+        return;
+      default: {
+        this.isContactSelected = true;
+
+        return;
+      }
+    }
+  }
+}
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,8 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  public constructor(private router: Router) {}
+
+  @Input()
+  public isHomeSelected: boolean = false;
+
+  @Input()
+  public isSoftwareSelected: boolean = false;
+
+  @Input()
+  public isDonateSelected: boolean = false;
+
+  @Input()
+  public isContactSelected: boolean = false;
+
+  public navigateToHome(): void {
+    this.router.navigateByUrl("/home");
+  }
+
+  public navigateToSoftware(): void {
+    this.router.navigateByUrl("/software");
+  }
+
+  public navigateToDonate(): void {
+    this.router.navigateByUrl("/donate");
+  }
+
+  public navigateToContact(): void {
+    this.router.navigateByUrl("/contact");
+  }
+}
