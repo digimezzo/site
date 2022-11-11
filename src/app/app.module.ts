24e1a9d5394226f672cc763c508151b2d98e547e
Digimezzo Raphaël <raphael@digimezzo.com>
2022-11-11 11:24:24 +0100

    Some tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { DonateComponent } from "./components/donate/donate.component";
+import { FooterComponent } from "./components/footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
 import { MenuComponent } from "./components/menu/menu.component";
+import { SocialMediaLinkComponent } from "./components/social-media-links/social-media-links.component";
 import { SoftwareComponent } from "./components/software/software.component";
 
 @NgModule({
@@ -17,6 +19,8 @@ import { SoftwareComponent } from "./components/software/software.component";
     DonateComponent,
     ContactComponent,
     MenuComponent,
+    FooterComponent,
+    SocialMediaLinkComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
---
+++ src/app/components/social-media-links/social-media-links.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-social-media-links",
+  templateUrl: "./social-media-links.component.html",
+  styleUrls: ["./social-media-links.component.scss"],
+})
+export class SocialMediaLinkComponent {}
