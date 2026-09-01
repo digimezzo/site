eca8da55ed38fc63268b3c13871fb0cd90d83bef
Digimezzo <digimezzo@outlook.com>
2026-09-01 07:37:42 +0200

    Update donation text

--- src/app/components/donate/donate.component.ts
+++ src/app/components/donate/donate.component.ts
@@ -12,13 +12,13 @@ const SOFTWARE_CONFIGS: { [key: string]: SoftwareConfig } = {
     name: "Dopamine",
     title: "Support Dopamine",
     description:
-      "If you enjoy Dopamine and would like to support its development, you can optionally send me a donation. The software remains completely free and there are no benefits or services attached to donating.",
+      "Enjoying Dopamine? If you'd like to support its continued development, you can leave a donation. Every contribution is greatly appreciated!",
   },
   knowte: {
     name: "Knowte",
     title: "Support Knowte",
     description:
-      "If you enjoy Knowte and would like to support its development, you can optionally send me a donation. The software remains completely free and there are no benefits or services attached to donating.",
+      "Enjoying Knowte? If you'd like to support its continued development, you can leave a donation. Every contribution is greatly appreciated!",
   },
 };
 
@@ -26,7 +26,7 @@ const DEFAULT_CONFIG: SoftwareConfig = {
   name: "Default",
   title: "Donate",
   description:
-    "If you enjoy the project and would like to support its development, you can optionally send me a donation. The software remains completely free and there are no benefits or services attached to donating.",
+    "Enjoying the project? If you'd like to support its continued development, you can leave a donation. Every contribution is greatly appreciated!",
 };
 
 @Component({
